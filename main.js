48865b5c6d385d324859c363b406fd1e17c516b0
Giuseppe <giusepperaffa1988@gmail.com>
2021-02-09 17:11:40 +0100

    Select- filter

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    tipi: ["All"],
+    generi: ["All"],
     dischi: []
   },
   //PROMEMORIA
@@ -14,11 +14,33 @@ var app = new Vue({
     axios
       .get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((result) => {
-        console.log(result);
+       console.log(result);
         // in dischi mettiamo tutto il contenuto del richiamo
         this.dischi = result.data.response;
-        console.log(this.dischi);
+        // console.log(this.dischi);
+
+        this.dischi.forEach((element,i) => {
+          console.log(element);
+          if(!this.generi.includes(element.genre)){
+            this.generi.push(element.genre);
+          }
+        });
       })
+      // messaggio di errore
       .catch(error => console.log('errore'));
+  },
+
+  // if (val == 'all') {
+  //  this.member = member;
+  //     } else{
+  // this.member = member.filter(function (e){ return e.gender == val;})
+  methods:{
+    filtraGeneri(){
+      if (val == "All"){
+        this.generi = generi;
+      }else{
+        this.generi = generi.filter(function (e) {return e.genre == val;})
+      }
+    }
   }
 });
