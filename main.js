6c64f6ea421da1d2c0dadca8654c26e7e917d16f
Giuseppe <giusepperaffa1988@gmail.com>
2021-02-09 16:16:58 +0100

    ok , manca bonus

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-$(document).ready(function() {
-	//Code
+var app = new Vue({
+  el: '#app',
+  data: {
+    tipi: ["All"],
+    dischi: []
+  },
+  //PROMEMORIA
+  // author: (...)
+  // genre: (...)
+  // poster: (...)
+  // title: (...)
+  // year: (...)
+  mounted(){
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((result) => {
+        console.log(result);
+        // in dischi mettiamo tutto il contenuto del richiamo
+        this.dischi = result.data.response;
+        console.log(this.dischi);
+      })
+      .catch(error => console.log('errore'));
+  }
 });
